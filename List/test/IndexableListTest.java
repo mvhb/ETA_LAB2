469e39d832cf5cedc017cb29b7e06033aa6ee837
Ronnie <souzasantos.ronnie@dal.ca>
2022-03-25 19:45:43 -0300

    late

---
+++ List/test/IndexableListTest.java
@@ -0,0 +1,54 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class IndexableListTest {
+
+    @Test
+    void acrescentar() {
+    }
+
+    @Test
+    void prefixar() {
+    }
+
+    @Test
+    void add() {
+    }
+
+    @Test
+    void aumentarEsquerdaArray() {
+    }
+
+    @Test
+    void aumentar() {
+    }
+
+    @Test
+    void contem() {
+    }
+
+    @Test
+    void get() {
+    }
+
+    @Test
+    void indexOf() {
+    }
+
+    @Test
+    void v() {
+    }
+
+    @Test
+    void remover() {
+    }
+
+    @Test
+    void set() {
+    }
+
+    @Test
+    void size() {
+    }
+}
